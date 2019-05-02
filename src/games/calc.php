73ae24135d276004167400b5b498c984866dfd9b
solar05 <artem00298@gmail.com>
2019-05-02 23:22:10 +0300

    Another refactoring

--- src/games/calc.php
+++ src/games/calc.php
@@ -6,32 +6,30 @@ use function BrainGames\gameEngine\playGame;
 const DESCRIPTION = 'What is the result of the expression?';
 const OPERATIONS = array('+', '-', '*', '/');
 const MAX_NUMBER = 50;
-const OPERATIONS_WITH_DIVISION = 3;
-const OPERATIONS_WITHOUT_DIVISION = 2;
 
 function playCalc()
 {
-    $executeOperations = array('+' =>
-    function ($a, $b) {
-        return $a + $b;
-    }, '-' =>
-    function ($a, $b) {
-        return $a - $b;
-    }, '*' =>
-    function ($a, $b) {
-        return $a * $b;
-    }, '/' =>
-    function ($a, $b) {
-        return $a / $b;
-    }
+    $executeOperations = array(
+        '+' => function ($a, $b) {
+            return $a + $b;
+        },
+        '-' => function ($a, $b) {
+            return $a - $b;
+        },
+        '*' => function ($a, $b) {
+            return $a * $b;
+        },
+        '/' => function ($a, $b) {
+            return $a / $b;
+        }
     );
     $getGameStage = function () use ($executeOperations) {
         $firstNumber = rand(1, MAX_NUMBER);
         $secondNumber = rand(1, MAX_NUMBER);
         if ($firstNumber % $secondNumber == 0) {
-            $chooseOperation = rand(0, OPERATIONS_WITH_DIVISION);
+            $chooseOperation = rand(0, count($executeOperations) - 1);
         } else {
-            $chooseOperation = rand(0, OPERATIONS_WITHOUT_DIVISION);
+            $chooseOperation = rand(0, count($executeOperations) - 2);
         }
         $currentOperation = OPERATIONS[$chooseOperation];
         $question = "$firstNumber $currentOperation $secondNumber";
