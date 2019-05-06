f5a0bba4f534f2434f3f109571803263def452e7
solar05 <artem00298@gmail.com>
2019-05-06 19:48:31 +0300

    Progression refactor

--- src/games/progression.php
+++ src/games/progression.php
@@ -7,34 +7,26 @@ const DESCRIPTION = 'What number is missing in the progression?';
 const PROGRESSION_SIZE = 10;
 const PROGRESSION_MAX_NUMBER = 50;
 
-function getProgression($firstElement, $progressionStep, $length)
+function getProgression($firstElement, $step, $length)
 {
     $progression = array();
     for ($counter = 0; $counter < $length; $counter += 1) {
-        $nextElement = $firstElement + $progressionStep * $counter;
+        $nextElement = $firstElement + $step * $counter;
         array_push($progression, $nextElement);
     }
     return $progression;
 }
 
-function hideProgressionElement($elementToHide, $progression)
-{
-    $hiddenElement = $progression[$elementToHide];
-    $progressionWithoutElement = $progression;
-    $progressionWithoutElement[$elementToHide] = '..';
-    return array($progressionWithoutElement, $hiddenElement);
-}
-
 function playProgression()
 {
     $getGameStage = function () {
-        $progressionStep = rand(1, PROGRESSION_MAX_NUMBER);
+        $step = rand(1, PROGRESSION_MAX_NUMBER);
         $firstProgressionElement = rand(1, PROGRESSION_MAX_NUMBER);
-        $progression = getProgression($firstProgressionElement, $progressionStep, PROGRESSION_SIZE);
-        $elementToHide = rand(0, PROGRESSION_SIZE - 1);
-        $proceededProgression = hideProgressionElement($elementToHide, $progression);
-        $question = implode(' ', $proceededProgression[0]);
-        $answer = $proceededProgression[1];
+        $progression = getProgression($firstProgressionElement, $step, PROGRESSION_SIZE);
+        $hiddenElementIndex = rand(0, PROGRESSION_SIZE - 1);
+        $answer = $progression[$hiddenElementIndex];
+        $progression[$hiddenElementIndex] = '..';
+        $question = implode(' ', $progression);
         return array($question, $answer);
     };
     playGame($getGameStage, DESCRIPTION);
